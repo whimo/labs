c93099bd9287812d589130a917a66be9de72de06
whimo <me@whimo.me>
2018-01-22 21:34:15 +0300

    Fix

--- 2_chromatography.py
+++ 2_chromatography.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 def process(filename, title):
     data = pandas.read_csv('res_down.csv')
 
-    data['t'] = pandas.Series([(t - data['t'][i - 1] if i != 0 else t) for i, t in enumerate(data['t'])])
+    data['t_dif'] = pandas.Series([(t - data['t'][i - 1] if i != 0 else t) for i, t in enumerate(data['t'])])
     data['x_norm'] = data['x'] * 0.001
 
-    data['v'] = pandas.Series([0.005 / t for t in data['t']])
-    data['delta_v'] = pandas.Series([(0.1 + (0.3 / t)) * (0.005 / t) for t in data['t']])
+    data['v'] = pandas.Series([0.005 / t for t in data['t_dif']])
+    data['delta_v'] = pandas.Series([(0.1 + (0.3 / t)) * (0.005 / t) for t in data['t_dif']])
 
     data['x2_norm'] = data['x_norm'] ** 2
     data['delta_x2'] = pandas.Series([(x ** 2) * (0.001 / x) for x in data['x_norm']])
@@ -25,8 +25,8 @@ def process(filename, title):
     plt.ylabel(u'$x^2,\, м^2$')
     plt.xlabel('$t$')
     for row in data.as_matrix():
-        plt.errorbar(x=row[1], y=row[5],
-                     xerr=0.3, yerr=row[6],
+        plt.errorbar(x=row[1], y=row[6],
+                     xerr=0.3, yerr=row[7],
                      label=True, color='#C00000')
 
     mng = plt.get_current_fig_manager()
@@ -37,8 +37,8 @@ def process(filename, title):
     plt.xlabel(u'$1/x,\, м^{-1}$')
     plt.ylabel(u'$V, м/с$')
     for row in data.as_matrix():
-        plt.errorbar(x=row[7], y=row[3],
-                     xerr=row[8], yerr=row[4],
+        plt.errorbar(x=row[8], y=row[4],
+                     xerr=row[9], yerr=row[5],
                      label=True, color='#0000C0')
 
     mng = plt.get_current_fig_manager()
